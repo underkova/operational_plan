c92076f28c95c6fa9a9138593e2aa1f66481e36b
underkova <leezock@yandex.ru>
2023-02-22 14:34:23 +0300

    try to return data from students DB into plan page

--- planner/students/views.py
+++ planner/students/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
+#from .models import students           remove #
+#students = students.objects.all()      remove #
+#{'students'}                           add this after line 8 after html file
 def students (request):
     return render (request, 'planner.html') #откроется файл разметки страницы мурата
 
