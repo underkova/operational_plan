573df7a91de3401030894e9592ec1bf6b116676a
underkova <leezock@yandex.ru>
2023-03-12 20:13:11 +0300

    new def for students

--- planner/instructors/views.py
+++ planner/instructors/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import instructors
 # Create your views here.
-def instructors_page ():
-    return HttpResponse('instructors page')
+def instructors (request):
+    return HttpResponse( 'instructors page')
