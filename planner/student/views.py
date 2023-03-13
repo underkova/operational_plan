20fd9a083e045d55d0140512790d14fbaad68882
underkova <leezock@yandex.ru>
2023-03-13 22:36:41 +0300

    fix: database (-s)

--- planner/student/views.py
+++ planner/student/views.py
@@ -3,14 +3,13 @@ from django.shortcuts import HttpResponse
 from .models import student
 from .models import student
 from django.shortcuts import render
-
 def student_list(request):
     student_names = student.objects.all().order_by('name')
     context = {'student_names': student_names}
     return render(request, 'planner.html', context)
 
 def index (request):
-    return HttpResponse('main page') #будет просто текст
+    return HttpResponse ('main page') #будет просто текст
 
 def about (request):
     return HttpResponse('description page') #тоже текст
