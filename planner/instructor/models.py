dece7da03478e1fc10d7475983bff52f950b5ad3
underkova <leezock@yandex.ru>
2023-03-13 23:33:00 +0300

    fix: return names/codes on preview

--- planner/instructor/models.py
+++ planner/instructor/models.py
@@ -8,10 +8,19 @@ class instructor (models.Model):
     instructor_flight_time = models.TimeField('Полетное время', max_length=100)
     aircraft_type = models.CharField('Тип ВС', max_length=10)
     instructor_type = models.CharField('Должность', max_length=30)
+
+    def __str__(self):
+        return self.instructor_name
 # Create your models here.
 
 class instructor_type (models.Model):
     instructor_type = models.CharField('Должность', max_length=30)
+
+    def __str__(self):
+        return self.instructor_type
 # Create your models here.
 class aircraft_type (models.Model):
     aircraft_type = models.CharField('Тип ВС', max_length=10)
+
+    def __str__(self):
+        return self.aircraft_type
