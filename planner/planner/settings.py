f2c5589570420dfdf24000253d1129452bc4a10b
underkova <leezock@yandex.ru>
2023-03-14 14:09:09 +0300

    feat: install django-bootstrap5

--- planner/planner/settings.py
+++ planner/planner/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'main',
     'student',
     'instructor',
+    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
