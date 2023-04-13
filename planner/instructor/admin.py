c8a1195f9b70092e8a58ee606a56b46aaf2e7634
PsychoMensis <103057687+PsychoMensis@users.noreply.github.com>
2023-04-13 19:51:21 +0300

    refactor: delete some code

--- planner/instructor/admin.py
+++ planner/instructor/admin.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
-#from .models import student
-#from .models import groups
 from .models import instructor
 from .models import instructor_type
 from .models import aircraft_type
 
-#admin.site.register(student)  # чтобы таблица студентов была видна в админке
-#admin.site.register(groups)
+
 admin.site.register(instructor)
 admin.site.register(instructor_type)
 admin.site.register(aircraft_type)
--- planner/instructor/models.py
+++ planner/instructor/models.py
@@ -12,11 +12,10 @@ class instructor (models.Model):
     def __str__(self):
         return self.instructor_name
     class Meta:
-
         verbose_name = 'Инструктор'
-        verbose_name_plural = 'Инструктора'  # вот эта штука не работает (Потому что эта штука должна быть внутри класса, а не вне. Теперь все работает
+        verbose_name_plural = 'Инструктора'
         ordering = ['instructor_name']
-# Create your models here.
+
 
 class instructor_type (models.Model):
     instructor_type = models.CharField('Должность', max_length=30)
@@ -26,15 +25,15 @@ class instructor_type (models.Model):
 
     class Meta:
         verbose_name = 'Должность'
-        verbose_name_plural = 'Должности'  # вот эта штука не работает (Потому что эта штука должна быть внутри класса, а не вне. Теперь все работает
+        verbose_name_plural = 'Должности'
         ordering = ['instructor_type']
-# Create your models here.
+
 class aircraft_type (models.Model):
     aircraft_type = models.CharField('Тип ВС', max_length=10)
 
     class Meta:
         verbose_name = 'Тип ВС'
-        verbose_name_plural = 'Типы ВС'  # вот эта штука не работает (Потому что эта штука должна быть внутри класса, а не вне. Теперь все работает
+        verbose_name_plural = 'Типы ВС'
         ordering = ['aircraft_type']
     def __str__(self):
         return self.aircraft_type
