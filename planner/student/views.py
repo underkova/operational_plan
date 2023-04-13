c905935e3cc394c7fa7db9f226b0ff5f6cab7796
PsychoMensis <103057687+PsychoMensis@users.noreply.github.com>
2023-04-13 17:00:04 +0300

    feat: Student action messages

--- planner/student/views.py
+++ planner/student/views.py
@@ -6,6 +6,7 @@ from .models import briefing
 from .models import exercise
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
@@ -17,13 +18,15 @@ __all__ = (
     'ops', 'list', 'StudDetailView', 'StudCreateView', 'StudUpdateView', 'StudDeleteView'
 )
 
+
 @login_required
 def ops(request):
     student_names = student.objects.all().order_by('name')
     brif = briefing.objects.all()
     ex = exercise.objects.all()
-    context = {'students': student_names, 'briefings': brif, 'exercises': ex,}
+    context = {'students': student_names, 'briefings': brif, 'exercises': ex, }
     return render(request, 'students.html', context)
+
 
 def list(request, pk=None):
     if request.method == 'POST':
@@ -33,27 +36,38 @@ def list(request, pk=None):
             form.save()
     form = StudForm()
     stud = student.objects.all()
-    lst = Paginator(stud, 2)
+    lst = Paginator(stud, 8)
     page_number = request.GET.get('page')
     page_obj = lst.get_page(page_number)
     context = {'page_obj': page_obj, 'form': form}
     return render(request, 'list.html', context)
 
+
 class StudDetailView(DetailView):
     queryset = student.objects.all()
     template_name = 'detail.html'
 
-class StudCreateView(CreateView):
+
+class StudCreateView(SuccessMessageMixin, CreateView):
     model = student
     form_class = StudForm
     template_name = 'create.html'
+    success_url = reverse_lazy('student:list')
+    success_message = "Студент успешно создан"
 
-class StudUpdateView(UpdateView):
+
+
+
+class StudUpdateView(SuccessMessageMixin, UpdateView):
     model = student
     form_class = StudForm
     template_name = 'update.html'
     success_url = reverse_lazy('student:list')
-class StudDeleteView(DeleteView):
+    success_message = "Студент успешно отредактирован"
+
+
+class StudDeleteView(SuccessMessageMixin, DeleteView):
     model = student
     template_name = 'delete.html'
     success_url = reverse_lazy('student:list')
+    success_message = "Студент успешно удален"
